31ac6b074671612d02aea945a729f5abcdd37267
LaRojaPlamya162 <124785235+LaRojaPlamya162@users.noreply.github.com>
2023-10-14 16:44:19 +0700

    Update SearchManagement.java

--- java/com/example/dictionary/SearchManagement.java
+++ java/com/example/dictionary/SearchManagement.java
@@ -8,77 +8,83 @@ import javafx.scene.control.TextField;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
+import org.controlsfx.control.textfield.AutoCompletionBinding;
+import org.controlsfx.control.textfield.TextFields;
+import javafx.util.Callback;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class SearchManagement {
-    FXMLLoader loader;
+    private FXMLLoader loader;
     private TextField textField;
     private WebView webView;
     private WebEngine webEngine;
     private Stage secondaryStage;
+    private List<String> suggestionList;
     protected ArrayList<String> searchedWord;
-    String ENGVIE_directory = "E:\\Coding\\Dictionary\\src\\main\\resources\\com\\example\\dictionary\\eng_vie.txt";
+    protected String ENGVIE_directory = "E:\\Coding\\Dictionary\\src\\main\\resources\\com\\example\\dictionary\\eng_vie.txt";
+
+    public SearchManagement() {
+        suggestionList = new ArrayList<>();
+    }
+
     public void EnglishTranslation() {
         secondaryStage = new Stage();
         secondaryStage.setTitle("Tra từ Tiếng Anh");
-        loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
-        try  {
+        loader = new FXMLLoader(getClass().getResource("searchManagement.fxml"));
+        try {
             Parent root = loader.load();
             textField = (TextField) loader.getNamespace().get("SearchTaskbar");
             Button searchButton = (Button) loader.getNamespace().get("searchButton");
-            searchButton.setOnAction(e ->loadWebPage());
+            searchButton.setOnAction(e -> loadWebPage());
             webView = (WebView) loader.getNamespace().get("translation");
             webEngine = webView.getEngine();
+
+            AutoCompletionBinding<String> auto = TextFields.bindAutoCompletion(textField, new Callback<AutoCompletionBinding.ISuggestionRequest, Collection<String>>() {
+                @Override
+                public Collection<String> call(AutoCompletionBinding.ISuggestionRequest iSuggestionRequest) {
+                    String userText = iSuggestionRequest.getUserText().toLowerCase();
+                    List<String> suggestions = suggestionList.stream()
+                            .filter(word -> word.toLowerCase().startsWith(userText))
+                            .collect(Collectors.toList());
+
+                    return suggestions;
+                }
+            });
+
             Scene secondaryScene = new Scene(root, 800, 600);
             secondaryStage.setScene(secondaryScene);
             secondaryStage.show();
-
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
     }
-    /*private void VietNamTranslation() {
-        secondaryStage = new Stage();
-        secondaryStage.setTitle("Cửa sổ thứ hai");
-        loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
-        try  {
-            Parent root = loader.load();
-            textField = (TextField) loader.getNamespace().get("SearchTaskbar");
-            Button searchButton = (Button) loader.getNamespace().get("searchButton");
-            searchButton.setOnAction(e ->loadWebPage());
-            webView = (WebView) loader.getNamespace().get("translation");
-            webEngine = webView.getEngine();
-            Scene secondaryScene = new Scene(root, 800, 600);
-            secondaryStage.setScene(secondaryScene);
-            secondaryStage.show();
-
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-        }
-    }*/
 
     private void loadWebPage() {
         String searchQuery = textField.getText();
         try (BufferedReader reader = new BufferedReader(new FileReader(ENGVIE_directory))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                // Load nội dung HTML vào WebView
                 String[] WORD = line.split("<");
-                if (searchQuery.equals(WORD[0]))
-                {
+                if (WORD[0].startsWith(searchQuery)) {
+                    suggestionList.add(WORD[0]);
+                }
+                if (searchQuery.equals(WORD[0])) {
                     webEngine.loadContent(line);
-                    break;
+                    searchedWord.add(WORD[0]);
                 }
             }
         } catch (IOException e) {
             System.err.println("Đã xảy ra lỗi khi đọc tệp: " + e.getMessage());
         }
-    }
-    void addHistory() {
-        searchedWord.add(textField.getText());
+        suggestionList = new ArrayList<>(new LinkedHashSet<>(suggestionList));
     }
 }
