e67c3e211d21bb0a565d0f1510ac3203b4183106
LaRojaPlamya162 <124785235+LaRojaPlamya162@users.noreply.github.com>
2023-10-14 16:46:15 +0700

    Update HelloController.java

--- java/com/example/dictionary/HelloController.java
+++ java/com/example/dictionary/HelloController.java
@@ -2,11 +2,13 @@ package com.example.dictionary;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.scene.web.WebView;
+
+import java.util.List;
+
 public class HelloController {
     @FXML
     public VBox welcomeText;
@@ -20,4 +22,36 @@ public class HelloController {
     @FXML
     public WebView translation;
 
+    @FXML
+    public Button ENGVIE;
+
+    @FXML
+    public Button VIEENG;
+
+    @FXML
+    public Button button1;
+
+    @FXML
+    public Button button2;
+
+    @FXML
+    public Button button3;
+
+    @FXML
+    public Button button4;
+
+    @FXML
+    public Button button5;
+
+    @FXML
+    public Button button6;
+
+    @FXML
+    public Button button7;
+
+    @FXML
+    public Button button8;
+
+    @FXML
+    public ListView listView;
 }
